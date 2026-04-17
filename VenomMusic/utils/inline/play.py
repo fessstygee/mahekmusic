489e22e26909d3271877bcfa60e544b77970d636
Venom Pratap <venompratap22@gmail.com>
2026-04-17 10:57:11 +0530

    venom

--- VenomMusic/utils/inline/play.py
+++ VenomMusic/utils/inline/play.py
@@ -66,6 +66,10 @@ def stream_markup_timer(_, chat_id, played, dur):
                 callback_data="GetTimer",
             )
         ],
+        [
+            InlineKeyboardButton(text="💬 sᴜᴘᴘᴏʀᴛ", url=SUPPORT_GROUP),
+            InlineKeyboardButton(text="📢 ᴄʜᴀɴɴᴇʟ", url=SUPPORT_CHANNEL),
+        ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
